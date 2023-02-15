100c90078e71b5887ecadc847e6ad627c6881d7d
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-02-15 20:46:18 +0530

    Made minor Changes in Views

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -5,9 +5,7 @@
 //  Created by Yashraj jadhav on 27/01/23.
 //
 
-import SwiftUI
-
-struct AddView: View {
+truct AddView: View {
     @State private var name = ""
     @State private var type = "Personal"
     @State private var amount = 0.0
@@ -24,29 +22,41 @@ struct AddView: View {
     var body: some View {
         NavigationView {
             Form {
-                TextField("Name", text: $name)
-
-                Picker("Type", selection: $type) {
-                    ForEach(types, id: \.self) {
-                        Text($0)
+                Section(header: Text("Expense Details")) {
+                    TextField("Name", text: $name)
+                    
+                    Picker("Type", selection: $type) {
+                        ForEach(types, id: \.self) {
+                            Text($0)
+                        }
                     }
                 }
-
-                TextField("Amount", value: $amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                 
-                    .keyboardType(.decimalPad)
+                Section(header: Text("Amount")) {
+                    TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                }
             }
-            .navigationTitle("Add new expense")
+            .navigationTitle("Add New Expense")
             .toolbar {
-                Button("Save") {
-                    let item = ExpenseItem(name: name, type: type, amount: amount)
-                    expenses.items.append(item)
-                    dismiss()
-                    
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        let item = ExpenseItem(name: name, type: type, amount: amount)
+                        expenses.items.append(item)
+                        dismiss()
+                    }
                 }
             }
         }
+        .accentColor(.blue)
+        .navigationBarTitleDisplayMode(.inline) // Show navigation title in line with the back button
     }
+
 }
 
 
