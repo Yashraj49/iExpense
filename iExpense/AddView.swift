4100ff917d876fc2507b1c831cc5af1d0c20f23d
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-03-16 23:10:36 +0530

    added app icon

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -13,53 +13,61 @@ struct AddView: View {
     @State private var amount = 0.0
     @State private var showingAddExpense = false
     
-    
-    
     @ObservedObject var expenses: Expenses
     @Environment(\.dismiss) var dismiss
-
 
     let types = ["Business", "Personal"]
 
     var body: some View {
         NavigationView {
-            Form {
-                Section(header: Text("Expense Details")) {
-                    TextField("Name", text: $name)
+            VStack {
+                Image("receipt")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 0)
+                
+                Form {
+                    Section(header: Text("Expense Details").font(.title2)) {
+                        TextField("Name", text: $name)
+                        
+                        Picker("Type", selection: $type) {
+                            ForEach(types, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        .font(.custom("Lato", size: 16))
+                    }
                     
-                    Picker("Type", selection: $type) {
-                        ForEach(types, id: \.self) {
-                            Text($0)
+                    Section(header: Text("Amount").font(.title2)) {
+                        TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            .keyboardType(.decimalPad)
+                            .font(.custom("Lato", size: 16))
+                    }
+                }
+                .padding(.vertical, 20)
+          
+                .navigationTitle("Add New Expense")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button("Cancel") {
+                            dismiss()
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button("Save") {
+                            let item = ExpenseItem(name: name, type: type, amount: amount)
+                            expenses.items.append(item)
+                            dismiss()
                         }
                     }
                 }
-                
-                Section(header: Text("Amount")) {
-                    TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                        .keyboardType(.decimalPad)
-                }
             }
-            .navigationTitle("Add New Expense")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button("Cancel") {
-                        dismiss()
-                    }
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("Save") {
-                        let item = ExpenseItem(name: name, type: type, amount: amount)
-                        expenses.items.append(item)
-                        dismiss()
-                    }
-                }
-            }
+            .accentColor(.blue)
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .accentColor(.blue)
-        .navigationBarTitleDisplayMode(.inline) // Show navigation title in line with the back button
     }
-
 }
+
 
 
 struct AddView_Previews: PreviewProvider {
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -10,70 +10,70 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingAddExpense = false
     @StateObject var expenses = Expenses()
-    @State private var animateopacity = false
-    
-    
+    @State private var animateOpacity = false
     
     var body: some View {
-        NavigationView{ 
-            List{
+        NavigationView {
+            List {
                 ForEach(expenses.items) { item in
                     HStack {
-                        VStack(alignment: .leading) {
+                        VStack(alignment: .leading, spacing: 8) {
                             Text(item.name)
                                 .font(.headline)
                             Text(item.type)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
                         }
-
                         Spacer()
                         Text(item.amount, format: .currency(code: "USD"))
+                            .font(.headline)
                             .foregroundColor(self.color(forAmount: Int(item.amount)))
                     }
                 }
                 .onDelete(perform: removeItems)
-                
-                
-            } // navigate from Home view to other .
-            .navigationBarTitle("iExpense")
-            .navigationBarItems(leading: EditButton(), trailing: Button(action: {self.showingAddExpense = true }) {
-               
-            })
-            
-            
-            .sheet(isPresented: $showingAddExpense) {
-                AddView(expenses: Expenses())
-                
             }
-            
-           
-            .toolbar {
-                Button {
-                    showingAddExpense = true
-                   
-                } label: {
-                    Image(systemName: "plus")
-                
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle("iExpense")
+            .navigationBarTitleDisplayMode(.large)
+            .navigationBarItems(
+                leading: EditButton(),
+                trailing: Button(action: { self.showingAddExpense = true }) {
+                    
                 }
+            )
+            .background(Color(.systemGroupedBackground))
+            .animation(.default)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: { self.showingAddExpense = true }) {
+                        Image(systemName: "plus")
+                            
+                          
+                    }
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
             }
         }
     }
   
-    func removeItems(at offsets : IndexSet)
-    {
+    func removeItems(at offsets: IndexSet) {
         expenses.items.remove(atOffsets: offsets)
-}
-    func color(forAmount amount: Int) -> Color {
-            switch amount {
-            case Int.min..<10:
-                return .green
-            case 10..<100:
-                return .orange
-            default:
-                return .red
-            }
-        }
+    }
     
+    func color(forAmount amount: Int) -> Color {
+        switch amount {
+        case Int.min..<10:
+            return .green
+        case 10..<100:
+            return .orange
+        default:
+            return .red
+        }
+    }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
